da33710ec5950217b8534b3db1faf982c516afea
debz-g <debz.exe@gmail.com>
2021-12-03 12:11:03 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,52 @@
+# YAWA-WeatherApp
+
+
+![GitHub issues](https://img.shields.io/github/issues/madhurmehta007/YAWA-YetAnotherWeatherApp?color=red&logo=Github)
+![GitHub](https://img.shields.io/github/license/madhurmehta007/YAWA-YetAnotherWeatherApp?color=green&logo=GitBook&logoColor=yellow)
+![GitHub top language](https://img.shields.io/github/languages/top/madhurmehta007/YAWA-YetAnotherWeatherApp?color=violet&logo=kotlin)
+
+YAWA is a simple single screen, single activity weather app with material UI.
+
+This repository is mainly focused on people who want to get started in open source with Android Development.
+
+Built with Kotlin and XML.
+
+# Getting Started
+
+- ### 💾 Cloning the Repository
+  
+  
+   • fork YAWA into your github.
+   
+   • cd into your preferred folder from CMD/Powerdhell/Terminal.
+   
+   • type the following command to clone the project in the folder.
+   
+      git clone repository_link
+      
+   • load up the project in android studio and now you are ready to work with code.
+      
+- ### ▶️ Contribution
+  
+  • After making necessary changes according the issue, commit and push the code directly from Android Studio.
+  
+  • Then open your forked github repository of YAWA.
+  
+  • Create a pull request for your code and wait for us to review your code and merge.
+
+
+# Get involved
+
+## ❤️ Community
+    
+  
+  [![Join Telegram Chat:https://t.me/+h_EZ6QprQss0MDVl](https://img.shields.io/badge/Telegram-2CA5E0?style=for-the-badge&logo=telegram&logoColor=white)](https://t.me/+h_EZ6QprQss0MDVl)
+  [![Join Discord Server:https://t.me/+h_EZ6QprQss0MDVl](https://img.shields.io/badge/Discord-7289DA?style=for-the-badge&logo=discord&logoColor=white)](https://t.me/+h_EZ6QprQss0MDVl)
+  
+  
+# Report Issues
+
+  If you faced some unknown issues and errors or some existing feature is not working, please report it in the Telegram Channel or Discord Server with exact steps of what you did
+  before the issue happened which basically means reproducing the error to solve it.
+  
+### In case of any other queries, feel free to ask in the groups and tag us for a faster reply.
---
+++ app/src/main/java/dev/yawa/weather/MainActivity.kt
@@ -0,0 +1,124 @@
+package dev.yawa.weather
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import dev.yawa.weather.databinding.ActivityMainBinding
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.Location
+import android.location.LocationManager
+import android.location.LocationRequest
+import android.os.Looper
+import android.util.Log
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.location.LocationManagerCompat.isLocationEnabled
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationCallback
+import com.google.android.gms.location.LocationResult
+import com.google.android.gms.location.LocationServices
+
+
+private lateinit var binding: ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var mfusedlocation: FusedLocationProviderClient
+    private var reqCode = 1010
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        mfusedlocation = LocationServices.getFusedLocationProviderClient(this)
+        getLastLocation()
+
+        binding.tempText.setOnClickListener {
+            getLastLocation()
+        }
+
+    }
+
+    private fun getLastLocation() {
+        if (checkPermission()) {
+            if (locationEnable()) {
+                mfusedlocation.lastLocation.addOnCompleteListener { task ->
+                    var location: Location? = task.result
+                    if (location == null) {
+                        newLocation()
+                    } else {
+                        Log.i("Location", location.longitude.toString())
+                    }
+                }
+            } else {
+                Toast.makeText(this, "Please Turn on GPS from settings", Toast.LENGTH_SHORT).show()
+            }
+        } else
+            requestPermission()
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun newLocation() {
+        var locationReq = com.google.android.gms.location.LocationRequest()
+        locationReq.priority =
+            com.google.android.gms.location.LocationRequest.PRIORITY_HIGH_ACCURACY
+        locationReq.interval = 0
+        locationReq.fastestInterval = 0
+        locationReq.numUpdates = 1
+        mfusedlocation = LocationServices.getFusedLocationProviderClient(this)
+        mfusedlocation.requestLocationUpdates(locationReq, locationCallBack, Looper.myLooper())
+    }
+
+    private val locationCallBack = object : LocationCallback() {
+        override fun onLocationResult(p0: LocationResult) {
+            var lastLocation: Location = p0.lastLocation
+        }
+    }
+
+    private fun locationEnable(): Boolean {
+        var locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || locationManager.isProviderEnabled(
+            LocationManager.NETWORK_PROVIDER
+        )
+    }
+
+    private fun requestPermission() {
+        ActivityCompat.requestPermissions(
+            this, arrayOf(
+                android.Manifest.permission.ACCESS_COARSE_LOCATION,
+                android.Manifest.permission.ACCESS_FINE_LOCATION
+            ), reqCode
+        )
+    }
+
+    private fun checkPermission(): Boolean {
+        if (
+            ActivityCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED ||
+            ActivityCompat.checkSelfPermission(
+                this,
+                android.Manifest.permission.ACCESS_COARSE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+        ) {
+            return true
+        }
+        return false
+    }
+
+    @SuppressLint("MissingSuperCall")
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == reqCode) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                getLastLocation()
+            }
+        }
+    }
+}
